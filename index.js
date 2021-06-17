af3e3839065096566233656c1f7263b07fd1902a
sidouxie <sid_ouxiee@hotmail.com>
2021-06-17 15:11:38 +0100

    ajoute du mini-serve

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+//Middleware
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("hello ");
+});
+
+app.listen(port, () => {
+  console.log(`server running at http://localhost:${port}`);
+});
